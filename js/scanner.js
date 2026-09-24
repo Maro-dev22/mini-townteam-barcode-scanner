f3c806e8ace9cc3fcc2564f34208365f0f58c446
Ahmed-dev <anaa17466@gmail.com>
2026-09-24 20:55:54 +0300

    Update Dynamsoft license key2

--- js/scanner.js
+++ js/scanner.js
@@ -17,7 +17,7 @@
 
 const CONFIG = {
     // Current Dynamsoft 30-day trial key.
-    LICENSE_KEY: "DLS2eyJoYW5kc2hha2VDb2RlIjoiMTA2MzcwNTczLU1UQTJNemN3TlRjekxYZGxZaTFVY21saGJGQnliMm8iLCJtYWluU2VydmVyVVJMIjoiaHR0cHM6Ly9tZGxzLmR5bmFtc29mdG9ubGluZS5jb20vIiwib3JnYW5pemF0aW9uSUQiOiIxMDYzNzA1NzMiLCJzdGFuZGJ5U2VydmVyVVJMIjoiaHR0cHM6Ly9zZGxzLmR5bmFtc29mdG9ubGluZS5jb20vIiwiY2hlY2tDb2RlIjoxMDc5NDI2MzQyfQ====",
+    LICENSE_KEY: "DLS2eyJoYW5kc2hha2VDb2RlIjoiMTA2MzcwNTg0LU1UQTJNemN3TlRnMExYZGxZaTFVY21saGJGQnliMm8iLCJtYWluU2VydmVyVVJMIjoiaHR0cHM6Ly9tZGxzLmR5bmFtc29mdG9ubGluZS5jb20vIiwib3JnYW5pemF0aW9uSUQiOiIxMDYzNzA1ODQiLCJzdGFuZGJ5U2VydmVyVVJMIjoiaHR0cHM6Ly9zZGxzLmR5bmFtc29mdG9ubGluZS5jb20vIiwiY2hlY2tDb2RlIjotMTk2NDg4ODY1Mn0=",
 
     // Fallback CDN (loaded only if Dynamsoft fails)
     HTML5QRCODE_CDN: "https://unpkg.com/html5-qrcode",
